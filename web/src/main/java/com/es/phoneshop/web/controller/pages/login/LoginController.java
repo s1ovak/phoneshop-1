6205dda77f531e3328a48b28f714522a84e0a656
s1ovak <45860242+s1ovak@users.noreply.github.com>
2019-12-29 13:43:51 +0300

    little fixes

--- web/src/main/java/com/es/phoneshop/web/controller/pages/login/LoginController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/login/LoginController.java
@@ -3,9 +3,10 @@ package com.es.phoneshop.web.controller.pages.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller(value = "/login")
+@Controller
+@RequestMapping(value = "/login")
 public class LoginController {
 
     @GetMapping
